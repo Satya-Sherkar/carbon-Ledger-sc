82fa5cd95a9c23f0c5e98db12d71f1b07301e2eb
Satyam Sherkar <satyamsherkar007@gmail.com>
2025-11-30 17:59:35 +0530

    Refactored CarbonCreditToken contract: improved error handling, updated function parameters, and enhanced documentation. Functions now validate receiver and amount before minting, burning, and approving tokens.

--- src/CarbonCreditToken.sol
+++ src/CarbonCreditToken.sol
@@ -3,34 +3,26 @@ pragma solidity ^0.8.30;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+error Not_Owner();
+error Invalid_receiver_or_amount();
+error Invalid_owner_or_amount();
+error Insufficient_owner_balance();
+
 /**
- *  @title CarbonCreditToken Test
- *  @author Satyam Sherkar
- *  @notice This contract implements an ERC20 token for carbon credits
- *  @dev Also see OpenZeppelin's ERC20 for inherited properties.
+ * @title Carbon Credit Token.
+ * @author Satyam Sherkar GitHub: https://github.com/Satya-Sherkar
+ * @notice This is a test contract. Under Development. 
  */
 contract CarbonCreditToken is ERC20 {
-    error InvalidAddress();
-    error InsufficientBalance();
-    error UnauthorizedAccount();
-
-    /**
-     *  @notice Address of the contract owner(marketplace contract)
-     */
+    // owner will be marketplace contract
     address public immutable OWNER;
 
-    /**
-     * @notice Initializes the contract with an owner address
-     * @dev Sets up the ERC20 token with name "Carbon Credit Token" and symbol "CCT"
-     * @param owner Address (Marketplace Contract) that will have permission to mint and burn tokens and all other functions of this contract.
-     */
+    // constructor
     constructor(address owner) ERC20("Carbon Credit Token", "CCT") {
         OWNER = owner;
     }
 
-    /**
-     * @notice Restricts function access to contract owner
-     */
+    // modifiers
     modifier onlyOwner() {
         _onlyOwner();
         _;
@@ -38,36 +30,32 @@ contract CarbonCreditToken is ERC20 {
 
     function _onlyOwner() internal view {
         if (msg.sender != OWNER) {
-            revert UnauthorizedAccount();
+            revert Not_Owner();
         }
     }
 
-    /**
-     * @notice Mints new tokens to a specified address
-     */
-    function mint(address to, uint256 amount) external onlyOwner {
-        if (to == address(0)) {
-            revert InvalidAddress();
+    // main functions
+    function mint(address receiver, uint256 amount) external onlyOwner {
+        if (receiver == address(0) || amount <= 0) {
+            revert Invalid_receiver_or_amount();
         }
-        _mint(to, amount);
+        _mint(receiver, amount * 1e18);
     }
 
-    /**
-     * @notice Burns tokens from a holder's address, use for retiring credits
-     * @param holder Address from which to burn tokens(Retire)
-     * @param amount Number of tokens to burn(Retire)
-     */
-    function burn(address holder, uint256 amount) external onlyOwner {
-        if (amount > balanceOf(holder)) {
-            revert InsufficientBalance();
+    function burn(address owner, uint256 amount) external onlyOwner {
+        if (owner == address(0) || amount <= 0) {
+            revert Invalid_owner_or_amount();
         }
-        _burn(holder, amount);
+        if (amount > balanceOf(owner)) {
+            revert Insufficient_owner_balance();
+        }
+        _burn(owner, amount * 1e18);
     }
 
-    /**
-     * @notice Approves spending of tokens on behalf of a holder, use for sending tokens for listing in marketplace contract.
-     */
-    function approve_(address holder, address spender, uint256 value) external onlyOwner {
-        _approve(holder, spender, value);
+    function approve(address owner, address spender, uint256 amount) external onlyOwner {
+        if (owner == address(0) || spender == address(0) || amount <= 0) {
+            revert Invalid_owner_or_amount();
+        }
+        _approve(owner, spender, amount * 1e18);
     }
 }
